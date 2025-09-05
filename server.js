5ec07d2f08f521a0d24059946d490e76d11f9dd4
Abdulloh Umar <iniautok@gmail.com>
2025-09-05 18:55:06 +0700

    Membuat HTTP Server secara Native

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+import http from 'http';
+
+const requestListener = (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+
+    res.statusCode = 200;
+    res.end('<h1>Hello HTTP Server!</h1>')
+}
+
+const server = http.createServer(requestListener);
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
