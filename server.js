9a2ba5bbe184a29d28cb4053f57f56fb2c919902
Abdulloh Umar <iniautok@gmail.com>
2025-09-05 23:02:18 +0700

    Method/Verb Request

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 import http from 'http';
+import method from 'http';
 
 const requestListener = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
-
     res.statusCode = 200;
-    res.end('<h1>Hello HTTP Server!</h1>')
+
+    const { method } = req;
+
+    if (method === 'GET') {
+        res.end('Ini adalah method GET');
+    }
+
+    if(method === 'POST') {
+        res.end('Ini adalah method POST');
+    }
+
+    if(method === 'PUT') {
+        res.end('Ini adalah method PUT');
+    }
+
+    if(method === 'DELETE') {
+        res.end('Ini adalah method DELETE');
+    }
 }
 
 const server = http.createServer(requestListener);
