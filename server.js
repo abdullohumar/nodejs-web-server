9d61a244ff687077e0b0fc6301b1ebd7a7c5ee25
Abdulloh Umar <iniautok@gmail.com>
2025-09-09 13:29:43 +0700

    Routing Request

--- server.js
+++ server.js
@@ -4,9 +4,41 @@ const requestListener = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.statusCode = 200;
 
-    const { method } = req;
+    const { method, url } = req;
 
-    if (method === 'GET') {
+    if(url === '/') {
+        // TODO 2: logika respons bila url bernilai '/'
+        if(method === 'GET') {
+            res.end('<h1>Ini halaman HOME PAGE</h1>')
+        }else {
+            res.end(`<h1>${method} tidak tersedia</h1>`)
+        }
+    } else if (url === '/about') {
+        // TODO 3: logika respons bila url bernilai '/about'
+        if(method === 'GET') {
+            res.end('<h1>Halo! Ini halaman about</h1>')
+        } else if(method === 'POST'){
+            let body = [];
+
+            req.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            req.on('end',() => {
+                body = Buffer.concat(body).toString();
+                const {name} = JSON.parse(body);
+                res.end(`<h1>Halo, ${name}! ini adalah halaman about</h1>`)
+            })
+        } else {
+            res.end(`<h1>${method} tidak tersedia</h1>`)
+        }
+    } else {
+        // TODO 4: logika respons bila url selain '/' dan '/about'
+        res.statusCode = 404;
+        res.end('<h1>Halaman tidak ditemukan!</h1>');
+    }
+
+    /*if (method === 'GET') {
         res.end('Ini adalah method GET');
     }
 
@@ -20,7 +52,7 @@ const requestListener = (req, res) => {
             const { name } = JSON.parse(body);
             res.end(`<h1>Hai, ${name}</h1>`)
         })
-    }
+    }*/
 
 }
 
