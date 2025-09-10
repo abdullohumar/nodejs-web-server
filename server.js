d47aff6415d394ec09c30b1cfbcb645a8b95539c
Abdulloh Umar <iniautok@gmail.com>
2025-09-10 13:49:05 +0700

    Response Body

--- server.js
+++ server.js
@@ -1,44 +1,67 @@
-import http from 'http';
+import http from "http";
 
 const requestListener = (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("X-Powered-By", "NodeJS");
+  res.statusCode = 200;
 
-    const { method, url } = req;
+  const { method, url } = req;
 
-    if(url === '/') {
-        // TODO 2: logika respons bila url bernilai '/'
-        if(method === 'GET') {
-            res.end('<h1>Ini halaman HOME PAGE</h1>')
-        }else {
-            res.end(`<h1>${method} tidak tersedia</h1>`)
-        }
-    } else if (url === '/about') {
-        // TODO 3: logika respons bila url bernilai '/about'
-        if(method === 'GET') {
-            res.end('<h1>Halo! Ini halaman about</h1>')
-        } else if(method === 'POST'){
-            let body = [];
-
-            req.on('data', (chunk) => {
-                body.push(chunk);
-            });
-
-            req.on('end',() => {
-                body = Buffer.concat(body).toString();
-                const {name} = JSON.parse(body);
-                res.end(`<h1>Halo, ${name}! ini adalah halaman about</h1>`)
-            })
-        } else {
-            res.end(`<h1>${method} tidak tersedia</h1>`)
-        }
+  if (url === "/") {
+    // TODO 2: logika respons bila url bernilai '/'
+    if (method === "GET") {
+      res.statusCode = 200;
+      res.end(
+        JSON.stringify({
+          message: "Ini Adalah HomePage",
+        })
+      );
     } else {
-        // TODO 4: logika respons bila url selain '/' dan '/about'
-        res.statusCode = 404;
-        res.end('<h1>Halaman tidak ditemukan!</h1>');
+      res.statusCode = 400;
+      res.end(
+        JSON.stringify({
+          message: "${method} tidak tersedia",
+        })
+      );
     }
+  } else if (url === "/about") {
+    // TODO 3: logika respons bila url bernilai '/about'
+    if (method === "GET") {
+      res.end(
+        JSON.stringify({
+          message: "Halo! Ini halaman about",
+        })
+      );
+    } else if (method === "POST") {
+      let body = [];
 
-    /*if (method === 'GET') {
+      req.on("data", (chunk1) => {
+        body.push(chunk1);
+      });
+
+      req.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        res.end(JSON.stringify({
+            message : `Halo, ${name}! ini adalah halaman about`
+        }));
+      });
+    } else {
+      res.end(JSON.stringify({
+        message : `${method} tidak tersedia`
+      }));
+    }
+  } else {
+    // TODO 4: logika respons bila url selain '/' dan '/about'
+    res.statusCode = 404;
+    res.end(
+      JSON.stringify({
+        message: "Halaman tidak ditemukan!",
+      })
+    );
+  }
+
+  /*if (method === 'GET') {
         res.end('Ini adalah method GET');
     }
 
@@ -53,13 +76,12 @@ const requestListener = (req, res) => {
             res.end(`<h1>Hai, ${name}</h1>`)
         })
     }*/
-
-}
+};
 
 const server = http.createServer(requestListener);
 const port = 5000;
-const host = 'localhost';
+const host = "localhost";
 
 server.listen(port, host, () => {
-    console.log(`Server berjalan pada http://${host}:${port}`);
+  console.log(`Server berjalan pada http://${host}:${port}`);
 });
